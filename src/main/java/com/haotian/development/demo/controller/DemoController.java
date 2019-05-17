f0d62a120cfa81e3cdaf07c48e6e82c69b05fc58
ZhangPeng <494009061@qq.com>
2019-05-18 02:48:18 +0800

    update

--- src/main/java/com/haotian/development/demo/controller/DemoController.java
+++ src/main/java/com/haotian/development/demo/controller/DemoController.java
@@ -1,19 +1,32 @@
 package com.haotian.development.demo.controller;
 
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Indexed;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * simple demo controller
+ *
  * @author: Mr.Zhang
  * @email zhangpeng@hiynn.com
  * @date: 2019/5/9 17:02
  */
-@Api(tags = "Demo API")
+@Api(tags = "spring-boot-web-demo")
 @RestController
-@RequestMapping(value = "demo", params = {"", ""})
+@RequestMapping("/demo")
 public class DemoController {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    @GetMapping("/")
+    public String index(){
+        log.debug("{}","input get index...");
+        return "this is simple spring boot demo controller";
+    }
 
 
 }
