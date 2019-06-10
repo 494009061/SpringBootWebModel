d8a99cd84a21afa615067a563523f4d8b797d33f
zhanghaotian <zhangpeng@hiynn.com>
2019-06-10 13:47:00 +0800

    update 20190610 1346

---
+++ src/main/java/com/haotian/development/demo/mapper/DemoTestMapper.java
@@ -0,0 +1,15 @@
+package com.haotian.development.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author: Mr.Zhang
+ * @email zhangpeng@hiynn.com
+ * @date: 2019/6/6 11:32
+ */
+@Mapper
+public class DemoTestMapper {
+
+
+
+}
